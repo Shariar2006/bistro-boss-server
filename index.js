04d5f942e5a1370e3d20191dc2c805c7511951ff
Shariar <shariarnafis86@gmail.com>
2023-11-16 19:30:46 +0600

    delete user data

--- index.js
+++ index.js
@@ -52,6 +52,15 @@ async function run() {
       res.send(result)
     })
 
+    
+
+    app.delete('/users/:id', async(req, res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await userCollection.deleteOne(query)
+      res.send(result)
+    })
+
     app.post('/users',async(req,res)=>{
       const user = req.body;
       const query = {email: user.email};
